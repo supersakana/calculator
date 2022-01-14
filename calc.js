c2b17eb223914fd49eaebebc669f209ff1af776e
supersakana <zacwilliamson@icloud.com>
2022-01-14 13:13:07 -0600

    Changed all of the div's into buttons to add value

--- calc.js
+++ calc.js
@@ -4,34 +4,18 @@
 let topText = document.getElementById('top-text');
 let bottomText = document.getElementById('bottom-text');
 
-const clearBtn = document.querySelector('.clear');
-const deleteBtn = document.querySelector('.delete');
-
-// NUMBERS
-const oneBtn = document.querySelector('.one');
-const twoBtn = document.querySelector('.two');
-const threeBtn = document.querySelector('.three');
-const fourBtn = document.querySelector('.four');
-const fiveBtn = document.querySelector('.five');
-const sixBtn = document.querySelector('.six');
-const sevenBtn = document.querySelector('.seven');
-const eightBtn = document.querySelector('.eight');
-const nineBtn = document.querySelector('.nine');
-const zeroBtn = document.querySelector('.zero');
-const deciBtn = document.querySelector('.deci');
-
-// OPERATORS
-const diviBtn = document.querySelector('.divi');
-const multiBtn = document.querySelector('.multi');
-const addBtn = document.querySelector('.add');
-const subBtn = document.querySelector('.sub');
-const equalBtn = document.querySelector('.equal');
 
 // ------------------------------
 // EVENTS
 
+
 // ------------------------------
 // FUNCTIONS
+
+// TEST
+function test(){
+    bottomText.innerText = btnOne.value;
+}
 
 
 // ADD
