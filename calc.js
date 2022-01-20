e972f1d22faa9d1195ca25bffb28f747c0679bf3
supersakana <zacwilliamson@icloud.com>
2022-01-20 11:43:53 -0600

    Added delete function

--- calc.js
+++ calc.js
@@ -1,12 +1,15 @@
-// ------------------------------
-// VARIABLES
+// VARIABLES------------------------------
 
+// SELECTED
 let topDisplay = document.getElementById('top-display');
 let bottomDisplay = document.getElementById('bottom-display');
 let numBtn = document.querySelectorAll('.num');
 let operBtn = document.querySelectorAll('.oper');
 let equalBtn = document.querySelector('.equal');
 let clearBtn = document.querySelector('.clear');
+let deleteBtn = document.querySelector('.delete')
+
+// VACCANT
 let newNum = [];
 let newOper = ''
 let total = 0;
@@ -14,8 +17,7 @@ let param1 = ''
 let param2 = ''
 let param3 = ''
 
-// ------------------------------
-// EVENTS
+// EVENTS------------------------------
 
 // Displays number on the calc screen
 numBtn.forEach(num => {
@@ -25,7 +27,7 @@ numBtn.forEach(num => {
     })
 })
 
-//When Operator is pressed
+// When Operator is pressed
 operBtn.forEach(oper => {
     oper.addEventListener('click', () => {
         param1 = bottomDisplay.innerText;
@@ -41,28 +43,27 @@ operBtn.forEach(oper => {
 })
 
 // When (=) is pressed
-    equalBtn.addEventListener('click', () => {
-        param1 = topDisplay.innerText;
-        param2 = bottomDisplay.innerText;
-        param3 = newOper;
-        operate(param1, param2, param3);
-        topDisplay.innerText = ''
-        newNum = [];
-        topDisplay.innerText = '';
-        bottomDisplay.innerText = total
+equalBtn.addEventListener('click', () => {
+    param1 = topDisplay.innerText;
+    param2 = bottomDisplay.innerText;
+    param3 = newOper;
+    operate(param1, param2, param3);
+    // total = digitLimit(total)
+
+    newNum = [];
+    topDisplay.innerText = '';
+    bottomDisplay.innerText = total
     })
 
-    // Clears calculator
-    clearBtn.addEventListener('click',() => {
-        newNum = [];
-        newOper = '';
-        topDisplay.innerText = '';
-        bottomDisplay.innerText = 0;
-        total = 0;
-    })
+// Deletes single value 
+deleteBtn.addEventListener('click', deleteMe);
+    
 
-// ------------------------------
-// FUNCTIONS
+// Clears calculator
+clearBtn.addEventListener('click', clear);
+
+
+// FUNCTIONS------------------------------
 
 // ADD
 function add(a, b){
@@ -82,7 +83,12 @@ function multiply(a, b){
 
 // DIVIDE
 function divide(a, b){
+    if(b != 0){
     return total = a / b;
+    } else {
+        alert('Infinity');
+        clear();
+    }
 }
 
 //OPERATOR
@@ -117,4 +123,33 @@ function operate(a, b, operator){
         divide(a, b);
         break;
    }
+}
+
+// DELETE
+function deleteMe() {
+    newNum = Array.from(bottomDisplay.innerText);
+    newNum.pop();
+    bottomDisplay.innerText = newNum.join("")  
+}
+
+// CLEAR
+function clear(){
+    newNum = [];
+        newOper = '';
+        topDisplay.innerText = '';
+        bottomDisplay.innerText = 0;
+        total = 0;
+}
+
+// DIGIT LIMIT
+function digitLimit(number){
+    numArr = Array.from(number);
+
+    if(numArr.length >= 11){
+        return number.toFixed(2);
+    } else {
+        return number
+    }
+    
+    
 }
