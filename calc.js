b9439eaeaa9699195a6400f5df8fa872ebc02ad8
Zac Williamson <89989651+supersakana@users.noreply.github.com>
2022-01-26 16:57:29 -0600

    Update calc.js
    Minor changes

--- calc.js
+++ calc.js
@@ -1,6 +1,4 @@
 // VARIABLES------------------------------
-
-// SELECTED
 let topDisplay = document.getElementById('top-display');
 let bottomDisplay = document.getElementById('bottom-display');
 let numBtn = document.querySelectorAll('.num');
@@ -8,19 +6,18 @@ let deciBtn = document.querySelector('.deci')
 let operBtn = document.querySelectorAll('.oper');
 let equalBtn = document.querySelector('.equal');
 let clearBtn = document.querySelector('.clear');
-let deleteBtn = document.querySelector('.delete')
+let deleteBtn = document.querySelector('.delete');
 
-// VACCANT
-let newNum = [];
-let newOper = ''
 let total = 0;
-let param1 = ''
-let param2 = ''
-let param3 = ''
+let newNum = [];
+let newOper = '';
+let param1 = '';
+let param2 = '';
+let param3 = '';
 
 // EVENTS------------------------------
 
-// Displays number on the calc screen
+// Displays number on screen
 numBtn.forEach(num => {
     num.addEventListener('click', () => {
         newNum += num.value;
@@ -32,15 +29,15 @@ numBtn.forEach(num => {
 operBtn.forEach(oper => {
     oper.addEventListener('click', () => {
         param1 = bottomDisplay.innerText;
-        param2 = topDisplay.innerText
+        param2 = topDisplay.innerText;
         if(param3 == ''){
             param3 = oper.value;
             operate(param1, param2, param3);
-            newOper = param3
+            newOper = param3;
         } else {
             operate(param1, param2, param3);
             param3 = oper.value;
-            newOper = param3
+            newOper = param3;
         }
         topDisplay.innerText = digitLimit(total);
         bottomDisplay.innerText = 0;
@@ -62,14 +59,10 @@ equalBtn.addEventListener('click', () => {
     deciBtn.disabled = false;
     })
 
+deciBtn.addEventListener('click', deciClick);
 
-// Decimal can only be clicked once
-deciBtn.addEventListener('click', deciClick)
-
-// Deletes single value 
 deleteBtn.addEventListener('click', deleteMe);
     
-// Clears calculator
 clearBtn.addEventListener('click', clear);
 
 
@@ -86,7 +79,6 @@ function subtract(a, b){
 }
 
 // MULTIPLY
-
 function multiply(a, b){
     return  total = a * b;
 }
@@ -103,20 +95,19 @@ function divide(a, b){
 
 //OPERATOR
 function operate(a, b, operator){
+    
     if(b == '' && operator == ' ÷ ' || b == '' && operator == ' * '){
         b = 1;
     }
-
     if( operator == ' - ' && b != ''){
         a = topDisplay.innerText;
         b = bottomDisplay.innerText;
     }
-
     if( operator == ' ÷ ' && b != 1){
         a = topDisplay.innerText;
         b = bottomDisplay.innerText;
     }
-        
+    
     a = Number(a);
     b = Number(b);
    switch(operator){
@@ -133,29 +124,29 @@ function operate(a, b, operator){
         divide(a, b);
         break;
    }
-   console.log(a, b, operator)
 }
 
 // DELETE
 function deleteMe() {
     let deleted = bottomDisplay.innerText.slice(0, -1);
     bottomDisplay.innerText = deleted;
+    
     for(let i = 0; i < deleted.length; i++){
         if(deleted[i] = '.'){
             deciBtn.disabled = false;
         }
     }
-    newNum = deleted
+    newNum = deleted;
 }
 
 // CLEAR
 function clear(){
     newNum = [];
-        newOper = '';
-        topDisplay.innerText = '';
-        bottomDisplay.innerText = 0;
-        total = 0;
-        deciBtn.disabled = false;
+    newOper = '';
+    topDisplay.innerText = '';
+    bottomDisplay.innerText = 0;
+    total = 0;
+    deciBtn.disabled = false;
 }
 
 // DIGIT LIMIT
@@ -164,7 +155,7 @@ function digitLimit(number){
         if(numArr.length >= 9){
         return number.toFixed(9);
         } else {
-            return number
+            return number;
         }
     }
 
